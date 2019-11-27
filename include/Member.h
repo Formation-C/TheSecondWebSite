cf0c1c2321962174196474ec2bd18d1b665a1964
JeanPhilippeCarrier <jeanphilippe.goux@carrier.com>
2019-11-27 15:15:15 +0100

    Initial commit

---
+++ include/Member.h
@@ -0,0 +1,18 @@
+#ifndef MEMBER_H
+#define MEMBER_H
+
+#include <User.h>
+
+
+class Member : public User
+{
+    public:
+        Member();
+        virtual ~Member();
+
+    protected:
+
+    private:
+};
+
+#endif // MEMBER_H
---
+++ include/User.h
@@ -0,0 +1,20 @@
+#ifndef USER_H
+#define USER_H
+
+
+class User
+{
+    public:
+        User();
+        virtual ~User();
+
+        string Getname() { return name; }
+        void Setname(string val) { name = val; }
+
+    protected:
+
+    private:
+        string name;
+};
+
+#endif // USER_H
